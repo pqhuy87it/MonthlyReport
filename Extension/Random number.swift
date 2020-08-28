b63c83c988cf214d548366e4fd378192c714fdd1
Phạm Quang Huy <pqhuy87it@gmail.com>
2020-08-28 15:34:17 +0900

    Create Random number.swift

---
+++ Extension/Random number.swift
@@ -0,0 +1,8 @@
+https://stackoverflow.com/questions/24007129/how-does-one-generate-a-random-number-in-apples-swift-language
+
+let randomInt = Int.random(in: 0..<6)
+let randomDouble = Double.random(in: 2.71828...3.14159)
+let randomBool = Bool.random()
+
+// Generates integers starting with 0 up to, and including, 10
+Int.random(in: 0 ... 10)
