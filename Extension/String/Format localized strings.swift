b38f378c1df7c37b105e72c5722a131c75d56261
Phạm Quang Huy <pqhuy87it@gmail.com>
2020-09-04 14:33:04 +0900

    Update Format localized strings.swift

--- Extension/String/Format localized strings.swift
+++ Extension/String/Format localized strings.swift
@@ -1,5 +1,7 @@
 https://stackoverflow.com/questions/35316655/how-to-format-localised-strings-in-swift
 
+https://stackoverflow.com/questions/24074479/how-to-create-a-string-with-format
+
 let x = 1.2
 let y = 2.4
 let text = String(format: NSLocalizedString("From %@, %@", comment: ""), "\(x)", "\(y)")
@@ -10,3 +12,19 @@ let text = String(format: NSLocalizedString("From %@, %@", comment: ""), NSNumbe
 with
 
 let text = String(format: NSLocalizedString("From %f, %f", comment: ""), x, y)
+
+-----
+
+String(format: "Value: %3.2f\tResult: %3.2f", arguments: [2.7, 99.8])
+or
+
+String(format:"Value: %3.2f\tResult: %3.2f", 2.7, 99.8)
+
+-----
+
+let timeNow = time(nil)
+let aStr = String(format: "%@%x", "timeNow in hex: ", timeNow)
+print(aStr)
+Example result:
+
+timeNow in hex: 5cdc9c8d
