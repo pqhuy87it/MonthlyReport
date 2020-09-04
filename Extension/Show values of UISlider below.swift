92515e146735ee6ab4ce5d7256b2635d93ea6313
Phạm Quang Huy <pqhuy87it@gmail.com>
2020-09-04 14:27:43 +0900

    Create Show values of UISlider below.swift

---
+++ Extension/Show values of UISlider below.swift
@@ -0,0 +1,4 @@
+https://stackoverflow.com/questions/35578256/how-can-you-show-values-of-uislider-below-it-on-screen-like-shown-in-the-picture/35579603#35579603
+
+![](https://i.stack.imgur.com/nuLer.png)
+
