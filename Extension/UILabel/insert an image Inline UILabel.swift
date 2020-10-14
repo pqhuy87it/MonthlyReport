99b442cdc269b3d07dba09fe2b71c1ec768b4b3a
Phạm Quang Huy <pqhuy87it@gmail.com>
2020-10-14 17:22:38 +0900

    Create insert an image Inline UILabel.swift

---
+++ Extension/UILabel/insert an image Inline UILabel.swift
@@ -0,0 +1,67 @@
+https://stackoverflow.com/questions/26744444/how-do-i-insert-an-image-inline-uilabel-in-ios-8-using-swift
+
+extension UILabel
+{
+    func addImage(imageName: String, afterLabel bolAfterLabel: Bool = false)
+    {
+        let attachment: NSTextAttachment = NSTextAttachment()
+        attachment.image = UIImage(named: imageName)
+        let attachmentString: NSAttributedString = NSAttributedString(attachment: attachment)
+
+        if (bolAfterLabel)
+        {
+            let strLabelText: NSMutableAttributedString = NSMutableAttributedString(string: self.text!)
+            strLabelText.appendAttributedString(attachmentString)
+
+            self.attributedText = strLabelText
+        }
+        else
+        {
+            let strLabelText: NSAttributedString = NSAttributedString(string: self.text!)
+            let mutableAttachmentString: NSMutableAttributedString = NSMutableAttributedString(attributedString: attachmentString)
+            mutableAttachmentString.appendAttributedString(strLabelText)
+
+            self.attributedText = mutableAttachmentString
+        }
+    }
+
+    func removeImage()
+    {
+        let text = self.text
+        self.attributedText = nil
+        self.text = text
+    }
+}
+
+----
+
+extension UILabel {
+
+    func addImageWith(name: String, behindText: Bool) {
+
+        let attachment = NSTextAttachment()
+        attachment.image = UIImage(named: name)
+        let attachmentString = NSAttributedString(attachment: attachment)
+
+        guard let txt = self.text else {
+            return
+        }
+
+        if behindText {
+            let strLabelText = NSMutableAttributedString(string: txt)
+            strLabelText.append(attachmentString)
+            self.attributedText = strLabelText
+        } else {
+            let strLabelText = NSAttributedString(string: txt)
+            let mutableAttachmentString = NSMutableAttributedString(attributedString: attachmentString)
+            mutableAttachmentString.append(strLabelText)
+            self.attributedText = mutableAttachmentString
+        }
+    }
+
+    func removeImage() {
+        let text = self.text
+        self.attributedText = nil
+        self.text = text
+    }
+}
